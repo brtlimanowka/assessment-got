39f062544c10919b29aca9722384c950cc5b60b4
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-11-08 16:35:58 +0100

    Add more columns

--- src/components/characters/CharactersItem.js
+++ src/components/characters/CharactersItem.js
@@ -5,39 +5,44 @@ const CharactersItem = ({ character }) => {
     .filter((nameOrAlias) => !!nameOrAlias)
     .join(', ');
   const columnAlive = () => {
-    const born = character.born;
-    const died = character.died;
+    const { born } = character;
+    const { died } = character;
 
+    if (born && died) {
+      const regex = new RegExp(/\d{1,}\s[A|B]/, 'gi');
+      const yearOfBirth = born
+        .match(regex)[0]
+        .replace(' ', '')
+        .replace('A', '')
+        .replace('B', '');
+      const yearOfDeath = died
+        .match(regex)[0]
+        .replace(' ', '')
+        .replace('A', '')
+        .replace('B', '');
+      return `No, died at ${yearOfDeath - yearOfBirth} years old`;
+    }
     if (!born && !died) {
       return 'Unknown';
     }
     if (!born) {
       return 'No';
     }
-    if (!died) {
-      return 'Yes';
-    }
-    const regex = new RegExp(/\d{1,}\s[A|b]/, 'gi');
-    const yearOfBirth = born
-      .match(regex)[0]
-      .replace(' ', '')
-      .replace('A', '')
-      .replace('B', '');
-    const yearOfDeath = died
-      .match(regex)[0]
-      .replace(' ', '')
-      .replace('A', '')
-      .replace('B', '');
-    return `No, died at ${yearOfDeath - yearOfBirth} years old`;
+    return 'Yes';
+  };
+  const columnCulture = character.culture ? character.culture : 'Unknown';
+  const columnAllegiances = () => {
+    const { allegiances } = character;
+    console.log(allegiances);
   };
 
   return (
     <div className='characters-item'>
       <span>{columnCharacter}</span>
       <span>{columnAlive()}</span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <span>{character.gender}</span>
+      <span>{columnCulture}</span>
+      <span>{columnAllegiances()}</span>
     </div>
   );
 };
