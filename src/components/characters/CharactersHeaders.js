275684e10f83d143d5ed07b3c5f6ac4515eb025f
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-11-09 16:01:34 +0100

    Handle filtering

--- src/components/characters/CharactersHeaders.js
+++ src/components/characters/CharactersHeaders.js
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import characterContext from '../../store/characters/characterContext';
 
 const CharactersHeaders = () => {
-  const [filteringCulture, setFilteringCulture] = useState(false);
+  const { filterByGender, filterByCulture, clearFilters } =
+    useContext(characterContext);
+  const [filteringGender, setFilteringGender] = useState(null);
+  const [filteringCulture, setFilteringCulture] = useState(null);
+
+  const genderSelectHandler = (event) => {
+    const selectedValue = event.target.value;
+    clearFilters();
+    if (selectedValue === 'Any') {
+      setFilteringGender(null);
+    } else {
+      filterByGender(selectedValue);
+      setFilteringGender(selectedValue);
+    }
+    if (filteringCulture) {
+      filterByCulture(filteringCulture);
+    }
+  };
+  const cultureTypeHandler = (event) => {
+    const enteredValue = event.target.value;
+    if (enteredValue) {
+      filterByCulture(enteredValue);
+      setFilteringCulture(enteredValue);
+    } else {
+      clearFilters();
+      setFilteringCulture(null);
+    }
+    if (filteringGender) {
+      filterByGender(filteringGender);
+    }
+  };
 
   return (
     <header>
@@ -10,7 +41,7 @@ const CharactersHeaders = () => {
       <span>
         Gender
         <div className='filter-gender'>
-          <select defaultValue='Any'>
+          <select defaultValue='Any' onChange={genderSelectHandler}>
             <option value='Any'>Any</option>
             <option value='Female'>Female</option>
             <option value='Male'>Male</option>
@@ -20,7 +51,7 @@ const CharactersHeaders = () => {
       <span>
         Culture
         <div className='filter-culture'>
-          <input id='culture' type='text' />
+          <input id='culture' type='text' onChange={cultureTypeHandler} />
           <label htmlFor='culture'>
             <i
               className={
