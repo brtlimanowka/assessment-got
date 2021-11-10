1aaba5941c3a260588848cccc25c99719c712ed9
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-11-10 12:50:14 +0100

    Add ErrorCard to House component

--- src/components/houses/House.js
+++ src/components/houses/House.js
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, Fragment } from 'react';
 import styled from 'styled-components';
 import houseContext from '../../store/houses/houseContext';
 import { useParams, useHistory } from 'react-router-dom';
+import ErrorCard from '../ui/ErrorCard';
 import HouseStyled from '../styles/House.styled';
 import Spinner from '../ui/Spinner';
 import Button from '../ui/Button';
@@ -16,7 +17,8 @@ const BackButton = styled(Button)`
 `;
 
 const House = () => {
-  const { startLoading, isLoading, house, getHouse } = useContext(houseContext);
+  const { startLoading, isLoading, house, getHouse, error } =
+    useContext(houseContext);
   let { id } = useParams();
   const history = useHistory();
   useEffect(() => {
@@ -26,56 +28,59 @@ const House = () => {
   }, []);
 
   return (
-    <HouseStyled>
-      {isLoading && <Spinner />}
-      {!isLoading && house && (
-        <div className='house-details'>
-          <header>
-            <h1>{house.name}</h1>
-            <h2>
-              <em>{house.words}</em>
-            </h2>
-          </header>
-          <article>
-            <ul>
-              <li>
-                <label>Region:</label>
-                <span>{house.region}</span>
-              </li>
-              <li>
-                <label>Coat of Arms:</label>
-                <span>{house.coatOfArms}</span>
-              </li>
-              <li>
-                <label>Titles:</label>
-                <span>
-                  {house.titles.map((title, index) => (
-                    <div key={index}>{title}</div>
-                  ))}
-                </span>
-              </li>
-              <li>
-                <label>Seats:</label>
-                <span>{house.seats.join(', ')}</span>
-              </li>
-              <li>
-                <label>Has died out:</label>
-                <span>{house.diedOut ? 'Yes' : 'No'}</span>
-              </li>
-              <li>
-                <label>Has overlord:</label>
-                <span>{house.overlord ? 'Yes' : 'No'}</span>
-              </li>
-              <li>
-                <label>Cadet branches:</label>
-                <span>{house.cadetBranches.length}</span>
-              </li>
-            </ul>
-          </article>
-          <BackButton onClick={() => history.goBack()}>Back</BackButton>
-        </div>
-      )}
-    </HouseStyled>
+    <Fragment>
+      {!isLoading && !!error && <ErrorCard />}
+      <HouseStyled>
+        {isLoading && <Spinner />}
+        {!isLoading && !error && house && (
+          <div className='house-details'>
+            <header>
+              <h1>{house.name}</h1>
+              <h2>
+                <em>{house.words}</em>
+              </h2>
+            </header>
+            <article>
+              <ul>
+                <li>
+                  <label>Region:</label>
+                  <span>{house.region}</span>
+                </li>
+                <li>
+                  <label>Coat of Arms:</label>
+                  <span>{house.coatOfArms}</span>
+                </li>
+                <li>
+                  <label>Titles:</label>
+                  <span>
+                    {house.titles.map((title, index) => (
+                      <div key={index}>{title}</div>
+                    ))}
+                  </span>
+                </li>
+                <li>
+                  <label>Seats:</label>
+                  <span>{house.seats.join(', ')}</span>
+                </li>
+                <li>
+                  <label>Has died out:</label>
+                  <span>{house.diedOut ? 'Yes' : 'No'}</span>
+                </li>
+                <li>
+                  <label>Has overlord:</label>
+                  <span>{house.overlord ? 'Yes' : 'No'}</span>
+                </li>
+                <li>
+                  <label>Cadet branches:</label>
+                  <span>{house.cadetBranches.length}</span>
+                </li>
+              </ul>
+            </article>
+            <BackButton onClick={() => history.goBack()}>Back</BackButton>
+          </div>
+        )}
+      </HouseStyled>
+    </Fragment>
   );
 };
 
