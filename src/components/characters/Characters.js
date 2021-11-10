3e867375c5593483c8a98cf505b2946047462445
Bartosz Limanówka <brtlimanowka@gmail.com>
2021-11-10 11:52:44 +0100

    Add ErrorCard display

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,31 +1,42 @@
 import React, { useContext, useEffect, Fragment } from 'react';
 import characterContext from '../../store/characters/characterContext';
 import CharactersTable from './CharactersTable';
+import ErrorCard from '../ui/ErrorCard';
 import CharactersPagnation from './CharactersPagination';
 import Spinner from '../ui/Spinner';
 import CharactersStyled from '../styles/Characters.styled';
 
 const Characters = () => {
-  const { startLoading, getCharacters, isLoading, characters, pagination } =
-    useContext(characterContext);
+  const {
+    startLoading,
+    getCharacters,
+    isLoading,
+    characters,
+    pagination,
+    error,
+  } = useContext(characterContext);
   useEffect(() => {
     startLoading();
     getCharacters();
     // eslint-disable-next-line
   }, []);
 
+  const renderContent = (
+    <Fragment>
+      <h1>Characters</h1>
+      {!isLoading && pagination && <CharactersPagnation />}
+      {!isLoading && characters && <CharactersTable />}
+    </Fragment>
+  );
+
   return (
-    <CharactersStyled>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <Fragment>
-          <h1>Characters</h1>
-          {pagination && <CharactersPagnation />}
-          {characters && <CharactersTable />}
-        </Fragment>
-      )}
-    </CharactersStyled>
+    <Fragment>
+      {!isLoading && !!error && <ErrorCard />}
+      <CharactersStyled>
+        {isLoading && <Spinner />}
+        {!isLoading && !error && renderContent}
+      </CharactersStyled>
+    </Fragment>
   );
 };
 
